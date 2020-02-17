1a691882d63147c26684b26ecbe2b886b09e7692
Henrique Costa Pessoa <hcostapessoa@gmail.com>
2020-02-17 16:59:43 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Landingpage from './Components/Landingpage';
+import Loginpage from './Components/Loginpage';
+import Signinpage from './Components/Signinpage';
+import Citiespage from './Components/Citiespage';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Landingpage}/>
+          <Route path="/loginpage" component={Loginpage}/>
+          <Route path="/signinpage" component={Signinpage}/>
+          <Route path="/citiespage" component={Citiespage}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Landingpage.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import Image from 'react-bootstrap/Image'
+import {Link} from "react-router-dom"
+
+export default class Landingpage extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="logoimage">
+                    <Image className="bg-image" src="/images/MYtineraryLogo.png" style = {{width: 350,height: 150}}/>
+                    </div>
+                    <div className="introduction">
+                    <p>Find Your perfect trip, designed by insiders who know and love their cities. </p>
+                    </div>
+                    <div className="startBrowsing">
+                    <h3>Start Browsing</h3>
+                    <Link id="citiespage" to="/citiespage">
+                    <Image className="bg-image" src="/images/circled-right-2.png" style = {{width: 150,height: 150}} />
+                    </Link>
+                    </div>
+                    <p>Want to build your own MYtinerary?</p>
+                    <div className="loginSignin">
+                    <Link id="login" to="/loginpage">Login in</Link>
+                    <Link id="signin" to="/signinpage">Sign in</Link>
+                    </div>
+                    <footer>
+                    <Image className="bg-image" src="/images/homeIcon.png" style = {{width: 50,height: 50}} />
+                    </footer>
+                </div>
+            </div>
+        )
+    }
+}
